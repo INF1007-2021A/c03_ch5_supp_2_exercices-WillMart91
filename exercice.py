5fe16a7808dd516e2891f7b2595dd731d242959e
WillMart91 <Willmartin91@outlook.com>
2021-09-27 12:51:24 -0400

    maanquedernier

--- exercice.py
+++ exercice.py
@@ -2,17 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import math
+from typing import List
 
 def get_num_letters(text):
-	return 0
+	cpt=0
+	for letter in text :
+
+		if letter.isalnum() :
+			cpt+=1
+	return cpt
 
 def get_word_length_histogram(text):
-	return [0]
+	word_list=text.split()
+	
+	
+	max_char=0
+	for word in word_list :
+		nb_current_char=get_num_letters(word)
+		if nb_current_char>max_char:
+			max_char=nb_current_char
+	
+	list_nb_char = []
+	for i in range(0,max_char+1):
+   		#list_nb_char[i] = 0
+		list_nb_char.append(0)
 
-def format_histogram(histogram):
+	
+	for word in word_list :
+		list_nb_char[get_num_letters(word)]+=1
+
+	#list_nb_char.
+	return list_nb_char
+
+def format_histogram(histogram: List):
 	ROW_CHAR = "*"
 
-	return ""
+	string=''
+	for number in range(0,len(histogram)):
+		char=''
+		for j in range (0,histogram[number]):
+			char+=ROW_CHAR
+		string+=(f"{number}: {char}\n")
+
+	return string
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
